afa38178de4a20d310ae488c8b5ba6ece9246a5e
Fabián <machucagelvez@gmail.com>
2026-05-06 19:03:57 -0500

    create user

--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,1 +1,31 @@
-export class User {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('varchar')
+  first_name: string;
+
+  @Column('varchar')
+  last_name: string;
+
+  @Column('varchar', { unique: true })
+  email: string;
+
+  @Column('varchar')
+  password: string;
+
+  @CreateDateColumn({ type: 'timestamptz' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ type: 'timestamptz' })
+  updatedAt: Date;
+}
